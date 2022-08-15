336824df4ae6fc4f22a0a95d95c85b3d5453ef3f
ryan <ryanjhigdon@gmail.com>
2022-08-14 23:17:56 -0230

    Modify DigitalOut to accept all GPIO parameters

--- src/gpio/include/gpio.h
+++ src/gpio/include/gpio.h
@@ -10,6 +10,27 @@ namespace gpio {
         PORTA = 0x48000000
     };
 
+    enum GpioSpeed
+    {
+        LOW_SPEED = 0,
+        MEDIUM_SPEED = 1,
+        HIGH_SPEED = 2,
+        VERY_HIGH_SPEED = 3
+    };
+
+    enum GpioOutType
+    {
+        PUSH_PULL = 0U,
+        OPEN_DRAIN = 1U
+    };
+
+    enum GpioPull
+    {
+        NO_PULL = 0,
+        PULL_UP = 1,
+        PULL_DOWN = 2
+    };
+
     /**
      * @brief gpio_base
      * 
@@ -39,10 +60,18 @@ namespace gpio {
     /**
      * @brief DigitalOut
      * 
-     * @tparam Port
+     * @tparam Port 
      * @tparam Pin 
+     * @tparam speed 
+     * @tparam type 
+     * @tparam pull 
      */
-    template < GpioPort Port, std::uint8_t Pin>
+    template <  GpioPort Port, 
+                std::uint8_t Pin,
+                GpioSpeed speed = GpioSpeed::MEDIUM_SPEED, 
+                GpioOutType type = GpioOutType::PUSH_PULL, 
+                GpioPull pull = GpioPull::NO_PULL
+                >
     struct DigitalOut
     {
         using gpiox = gpio_base<Port>;
@@ -54,6 +83,7 @@ namespace gpio {
         using pin_speed = cppreg::Field<typename gpiox::ospeedr, 2u, Pin*2, cppreg::read_write>;
         using pin_pull = cppreg::Field<typename gpiox::pupdr, 2u, Pin*2, cppreg::read_write>;
         using pin_set = cppreg::Field<typename gpiox::bsrr, 1u, Pin, cppreg::read_write>;
+        using pin_type = cppreg::Field<typename gpiox::otyper, 1u, Pin, cppreg::read_write>;
         using pin_clear = cppreg::Field<typename gpiox::bsrr, 1u, Pin + 16, cppreg::read_write>;
 
         // // We can now define the static methods of the interface.
@@ -70,8 +100,9 @@ namespace gpio {
         inline static void init() {
             off();
             pin_direction::write(1U);
-            pin_speed::write(0U);
-            pin_pull::write(0U);
+            pin_speed::write(speed);
+            pin_type::write(type);
+            pin_pull::write(pull);
         };        
     };
 }
